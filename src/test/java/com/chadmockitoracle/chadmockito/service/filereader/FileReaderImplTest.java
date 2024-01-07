2aeafacc3ffb64551eb8751137b3b391ed41b9d8
madhukarGit <hmkaws428@gmail.com>
2024-01-07 09:53:07 +0530

    second commit

--- src/test/java/com/chadmockitoracle/chadmockito/service/filereader/FileReaderImplTest.java
+++ src/test/java/com/chadmockitoracle/chadmockito/service/filereader/FileReaderImplTest.java
@@ -1,25 +1,21 @@
 package com.chadmockitoracle.chadmockito.service.filereader;
 
-import com.chadmockitoracle.chadmockito.entity.HousingProduction;
 import com.chadmockitoracle.chadmockito.repository.HousingRepository;
 import com.chadmockitoracle.chadmockito.service.emailsender.EmailServiceImpl;
-import jakarta.mail.Session;
-import jakarta.mail.internet.MimeMessage;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.util.ReflectionTestUtils;
 
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
-@SpringBootTest
+import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class FileReaderImplTest {
     @InjectMocks
     public FileReaderImpl fileReader;
@@ -29,27 +25,12 @@ class FileReaderImplTest {
     @Mock
     private EmailServiceImpl emailService;
 
-    @Mock
-    private JavaMailSender javaMailSender;
-
-    private MimeMessage mimeMessage;
-
     @BeforeEach
-    public void setUp(){
-        HousingProduction housingProduction = new HousingProduction();
-
-        when(javaMailSender.createMimeMessage()).thenReturn(new MimeMessage((Session) null));
-        doNothing().when(javaMailSender).send(any(MimeMessage.class));
-
-        when(housingRepository.saveAll(any())).thenReturn(List.of(housingProduction));
-
-        when(emailService.sendMailWithAttachment(any(/**/))).thenReturn(any());
-
-        fileReader = new FileReaderImpl(emailService,housingRepository);
-
-        ReflectionTestUtils.setField(fileReader,"batchSize",50);
-
-
+    public void setUp() {
+        ReflectionTestUtils.setField(fileReader, "batchSize", 50);
+        when(housingRepository.saveAll(any())).thenReturn(any());
+        when(emailService.sendMailWithAttachment(any())).thenReturn(anyString());
+        //fileReader = new FileReaderImpl(emailService, housingRepository);
     }
 
 
